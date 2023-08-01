bd8a418f03dc5580c52c1f30c13ec062a0fda861
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2023-08-01 12:19:46 +0300

    feat: Product model was created

---
+++ directus/expressproject/src/models/products.ts
@@ -0,0 +1,17 @@
+
+// collection A
+interface Products {
+    id: number;
+    status: string;
+    sort: number;
+    date_created: Date;
+    date_updated: Date;
+    name: string;
+    description: string;
+    price: number;
+  
+    // relations
+    m2m: number[] | Products_Categories[];
+  }
+
+  export default Products;
