fd1512bff89b9a0454943893d4f123662a14534f
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2023-08-01 10:43:58 +0300

    feat: The 'dotenv' package has been installed, and the configuration file has been created

--- directus/expressproject/src/app.ts
+++ directus/expressproject/src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
-const port = 4000;
 
-app.listen(port, () => {
+dotenv.config();
+
+app.listen(process.env.PORT, () => {
     console.log("The server is running..");
 });
